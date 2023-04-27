dfd6c069cfa05b8460a3c89ddcf7904c692b459e
TJ <tj202304@gmail.com>
2023-04-27 22:05:26 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    var answer = 0;
+    
+    if (a%2 === 1 && b%2 === 1) {
+        answer = a*a + b*b;
+    } else if (a%2 === 0 && b%2 === 0) {
+        answer = Math.abs(a-b);
+    } else {
+        answer = 2*(a + b);
+    }
+    
+    
+    return answer;
+}
