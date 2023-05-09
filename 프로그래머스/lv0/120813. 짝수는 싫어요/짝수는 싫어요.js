7d315a2b938b120bec60026adf23bf3cfc9f3d3c
TJ <tj202304@gmail.com>
2023-05-09 23:31:27 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = [];
+    
+    for (i=1; i <= n; i++ ) {
+        if (i%2 !== 0 ) {
+            answer.push(i)
+        }  
+    };
+    
+    return answer;
+}
