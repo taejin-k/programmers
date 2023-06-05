a3948505f6c08e692ed2e3419b247ecf371f229d
TJ <tj202304@gmail.com>
2023-06-05 13:18:38 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -4,7 +4,7 @@ function solution(arr, idx) {
     for (let i=0; i<arr.length; i++) {
         if (i >= idx && arr[i] === 1) {
             answer = i
-            break;
+            break;;
         }
         
     }
