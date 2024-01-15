62a650a0a0e3a29b2197cffeec9e3efaae9fc9d5
TJ <tj202304@gmail.com>
2024-01-15 21:53:39 +0900

    [level Lv.0] Title: 2의 영역, Time: 4.41 ms, Memory: 41.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181894. 2의 영역/2의 영역.js
@@ -0,0 +1,22 @@
+function solution(arr) {
+    var answer = [];
+    let indexArr = [];
+
+    for(i=0; i<arr.length; i++) {
+        if(String(arr[i]).includes('2')) {
+            indexArr.push(i);
+        }
+    }
+    
+    if (indexArr[0] === undefined) {
+        return [-1]
+    }
+    
+    if (indexArr[0] === indexArr.at(-1)) {
+        return [arr[indexArr[0]]]
+    }
+    
+    answer = arr.slice(indexArr[0], indexArr.at(-1) + 1);
+    
+    return answer;
+}
