9cf6303a1a7d3c7e2e07df63e5e1da44866fe83f
TJ <tj202304@gmail.com>
2024-06-18 20:07:46 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,12 @@
+function solution(food) {
+    var answer = '';
+    
+    for (let i=1; i<food.length; i++) {
+        answer = answer + String(i).repeat(Math.floor(food[i]/2))
+    }
+    
+    answer = answer + '0' + [...answer].reverse().join('')
+    
+    
+    return answer;
+}
