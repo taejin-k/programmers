5cc86ba628ee6113b1aa19d23c2c17f52dfcbecd
TJ <tj202304@gmail.com>
2024-06-19 11:12:54 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,19 +1,14 @@
 function solution(cards1, cards2, goal) {
     var answer = 'Yes';
-    let tempGoal = [...goal]
     
-    for (let i=0; i<goal.length; i++) {
-        console.log(cards1, cards2)
-        
-        if (cards1[0] === tempGoal[0]) {
+    for (let i=0; i<goal.length; i++) {        
+        if (cards1[0] === goal[i]) {
             cards1.shift();
-            tempGoal.shift();
             continue;
         }
         
-        if (cards2[0] === tempGoal[0]) {
+        if (cards2[0] === goal[i]) {
             cards2.shift();
-            tempGoal.shift();
             continue;
         } 
         
