8d3bf53b1208d7dddb0c017d655f47aa65ec92e2
TJ <tj202304@gmail.com>
2024-06-21 11:04:41 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,11 +1,10 @@
 function solution(n) {
     var answer = '';
+    let label = "수박";
     
-    if (n%2 === 0) {
-         answer = '수박'.repeat(n/2)
-    } else {
-        answer = '수박'.repeat(Math.floor(n/2)) + '수'
-    }
-   
+    if (n%2 === 0) answer = label.repeat(n/2);
+    else answer = label.repeat(Math.floor(n/2)) + '수'
+    
+    
     return answer;
 }
