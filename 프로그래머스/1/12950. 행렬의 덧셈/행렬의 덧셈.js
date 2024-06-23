cb5fa67871439877999386c213461ca7d6f19700
TJ <tj202304@gmail.com>
2024-06-23 21:59:11 +0900

    [level 1] Title: 행렬의 덧셈, Time: 12.67 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -2,15 +2,14 @@ function solution(arr1, arr2) {
     var answer = [];
     
     for (let i=0; i<arr1.length; i++) {
-        let result = [];
+        let array = [];
         
         for (let j=0; j<arr1[0].length; j++) {
-            result.push(arr1[i][j] + arr2[i][j])
+            array.push((arr1[i][j] + arr2[i][j]));
         }
         
-        answer.push(result)
+        answer.push(array)
     }
-    
-    
+
     return answer;
 }
