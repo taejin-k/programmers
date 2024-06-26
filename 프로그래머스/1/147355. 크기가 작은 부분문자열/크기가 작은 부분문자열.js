fc69ba0a402a7d610381b8851768f065a1c1df9e
TJ <tj202304@gmail.com>
2024-06-26 17:14:02 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,12 +1,11 @@
 function solution(t, p) {
     var answer = 0;
     
-    for (let i=0; i<t.length -p.length+1; i++) {
-        
-       if (Number(t.slice(i, i+p.length)) <= Number(p)) {
-           console.log(Number(t.slice(i, i+p.length)))
-           answer++
-       }
+    for (let i=0; i<t.length - (p.length - 1); i++) {
+        if (Number(p) >= Number(t.slice(i, i+p.length))) {
+            answer++
+        }    
     }
+    
     return answer;
 }
